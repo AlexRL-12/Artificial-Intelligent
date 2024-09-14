563af1c4961ff9a668e3d2e59b036a1951157b45
AlexisESuarez <ale.xsuuarez@gmail.com>
2024-09-14 11:48:40 -0500

    Programa Funcionando

---
+++ Sistema-de-reconocimiento-facial-y-Liveness/prueba.py
@@ -0,0 +1,13 @@
+
+# Libreries
+from tkinter import *
+import os
+
+
+# imagenF = PhotoImage(file = os.path.join(os.getcwd(),"./SetUp/Inicio.png"))
+
+# print(os.path.join(os.getcwd(),"./SetUp/Inicio.png"))
+
+print("************************")
+ruta = (os.getcwd()+'\SetUp\Inicio.png').replace("\\","/")
+print(os.getcwd())
