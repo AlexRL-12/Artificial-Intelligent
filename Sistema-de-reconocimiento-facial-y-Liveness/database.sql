086e116e333b93a59179554f7f092a20fcc0f2a1
Juan David <jdmm360@gmail.com>
2024-10-12 10:43:33 -0500

    feat: include database model

---
+++ Sistema-de-reconocimiento-facial-y-Liveness/database.sql
@@ -0,0 +1,26 @@
+-- Active: 1728745940946@@127.0.0.1@5433@facial_recognition
+CREATE TABLE "house" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "address" varchar NOT NULL
+);
+
+CREATE TABLE "user" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "name" varchar NOT NULL,
+  "username" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "file_path" varchar NOT NULL,
+  "house_id" int NOT NULL
+);
+
+CREATE TABLE "entrance" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "user_id" integer,
+  "date" timestamp NOT NULL,
+  "file_path" varchar NOT NULL,
+  "allowed" boolean
+);
+
+ALTER TABLE "user" ADD FOREIGN KEY ("house_id") REFERENCES "house" ("id");
+
+ALTER TABLE "entrance" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
